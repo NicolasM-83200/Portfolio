f8852419ee32c0269d0027490b9fa338181ea2da
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:26:05 +0100

    ajout middleware email et password

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userCtrl = require("../controllers/user.controller");
+const validatePassword = require("../middleware/password-validator");
+const validateEmail = require("../middleware/email-validator");
 
-router.post("/signup", userCtrl.signup);
+router.post("/signup", validateEmail, validatePassword, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
