39be34ab8aeaef6d9e4f61f4e957833d9af77f14
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:41:26 +0100

    création du composant Modal servant a afficher les projets

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import InfoProject from "./InfoProject";
+import { getProjects } from "../lib/common";
+
+const Modal = () => {
+  const modalRef = useRef();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const observeRefValue = modalRef.current;
+    disableBodyScroll(observeRefValue);
+    return () => {
+      if (observeRefValue) {
+        enableBodyScroll(observeRefValue);
+      }
+    };
+  }, []);
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function getProjectsList() {
+      const data = await getProjects();
+      if (data) {
+        setProjects(data);
+      }
+    }
+    getProjectsList();
+  }, []);
+
+  const project = projects.find((project) => project._id === id);
+  console.log(project);
+
+  return (
+    <div
+      ref={modalRef}
+      className="fixed inset-0 z-[1] flex items-center justify-center backdrop-blur"
+      onClick={() => navigate("/")}
+    >
+      <div
+        className="fixed flex w-[80vw] max-w-[1024px] flex-col items-center justify-center rounded-lg bg-quaternary p-6 shadow-xl"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <InfoProject projectData={project} />
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
