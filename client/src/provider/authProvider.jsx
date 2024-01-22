2678d75944dee23baafe0188f29ea1b67a5b669c
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:32:57 +0100

    ajout provider pour gestion état authentification

---
+++ client/src/provider/authProvider.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  // state to hold the authentication token
+  const [token, setToken_] = useState(localStorage.getItem("token"));
+
+  // Fonction to set the authentication token
+  const setToken = (newToken) => {
+    setToken_(newToken);
+  };
+
+  useEffect(() => {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      localStorage.setItem("token", token);
+    } else {
+      delete axios.defaults.headers.common["Authorization"];
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
+  // Memoized value of the authentication context
+  const contextValue = useMemo(() => {
+    return { token, setToken };
+  }, [token]);
+
+  // Provide the authentication context to the children coponents
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export default AuthProvider;
