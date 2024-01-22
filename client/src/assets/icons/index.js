3024907db3f5c2d64c4dbbe17f054910ace9c42d
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:31:02 +0100

    ajout icones et images

---
+++ client/src/assets/icons/index.js
@@ -0,0 +1,10 @@
+import html from "./html.png";
+import css from "./css.png";
+import js from "./javascript.png";
+import react from "./react.png";
+import tailwind from "./tailwind.png";
+import node from "./node.png";
+import express from "./express.png";
+import mongodb from "./mongodb.png";
+
+export { html, css, js, react, tailwind, node, express, mongodb };
