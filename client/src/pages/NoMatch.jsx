ebcf3b0c40a14c2717a0c2f737dab8fd24a02b22
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:36:04 +0100

    création page erreur

---
+++ client/src/pages/NoMatch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NoMatch = () => {
+  return (
+    <div className="self grid min-h-screen items-center justify-items-center">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-9xl">404</h1>
+        <h2 className="text-4xl">Oups... La page demandée n'existe pas</h2>
+        <Link to={"/"}>Retour à l'accueil</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NoMatch;
