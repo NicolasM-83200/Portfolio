f6ef0024b43af20b00b9bb4bd3f3196f1f8bbc9d
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:39:23 +0100

    ajout section 'about'

---
+++ client/src/components/sections/About.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section
+      id="about"
+      className="flex-col items-center justify-evenly border-t-[1px] border-primary bg-secondary px-2 lg:px-24"
+    >
+      <h2>A propos</h2>
+      <p>
+        Depuis mes premiers pas dans le monde du développement, j'ai consacré
+        mon temps à perfectionner mes compétences et à explorer les dernières
+        tendances de l'industrie. Durant mon parcours de formation avec
+        OpenClassrooms, j'ai acquis une base solide en informatique qui m'a
+        permis de naviguer avec confiance dans le paysage dynamique du
+        développement web.
+      </p>
+      <p>
+        Ce qui me motive chaque jour, c'est ma passion pour le développement.
+        Pour moi, coder va au-delà d'une simple tâche professionnelle :{" "}
+        <strong>c'est une véritable passion.</strong> J'aime explorer de
+        nouvelles idées, résoudre des problèmes stimulants et créer des
+        expériences en ligne exceptionnelles. Chaque ligne de code que j'écris
+        est une opportunité d'innover et de contribuer positivement à la vie des
+        utilisateurs.
+      </p>
+    </section>
+  );
+};
+
+export default About;
