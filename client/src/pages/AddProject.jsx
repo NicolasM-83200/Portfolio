9917fd77faaa1421e14d776d57fe57ac6cc243ff
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-22 14:35:27 +0100

    création page ajout de projet

---
+++ client/src/pages/AddProject.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import ProjectForm from "../components/ProjectForm";
+import { Link } from "react-router-dom";
+import BackArrow from "../components/BackArrow";
+
+const AddProject = () => {
+  const [created, setCreated] = useState(false);
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <BackArrow />
+      {!created ? (
+        <>
+          <h2>Ajouter un projet</h2>
+          <p>Tous les champs sont obligatoires</p>
+          <ProjectForm validate={setCreated} />
+        </>
+      ) : (
+        <>
+          <h2>Merci !</h2>
+          <p>Votre projet à bien été ajouté</p>
+          <Link to="/">Retour à l'accueil</Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AddProject;
