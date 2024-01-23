97708af98f063b62d5f51fb6d7032ccce2660d99
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-01-23 16:30:47 +0100

    update formulaire contact

--- client/src/components/FormContact.jsx
+++ client/src/components/FormContact.jsx
@@ -1,37 +1,101 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const FormContact = () => {
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  const form = useRef(null);
+  const [notif, setNotif] = useState({ error: false, message: "" });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_p61h8vk",
+        "template_98qdy2m",
+        form.current,
+        "UAExXDmOT69yE6Yy5",
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setNotif({
+            error: false,
+            message: "Merci! Votre message a bien été envoyé.",
+          });
+          form.current.reset();
+          setTimeout(() => {
+            setNotif({ error: false, message: "" });
+          }, 3000);
+        },
+        (error) => {
+          console.log(error.text);
+          setNotif({
+            error: true,
+            message:
+              "Désolé, une erreur s'est produite, veuillez réessayer plus tard.",
+          });
+          setTimeout(() => {
+            setNotif({ error: false, message: "" });
+          }, 3000);
+        },
+      );
+  };
+
+  const errorClass = notif.error
+    ? "text-red-500 bg-red-200 border-2 border-red-500 rounded-md"
+    : "text-orange-500 bg-orange-200 border-2 border-orange-500 rounded-md";
 
   return (
-    <form className="flex h-full w-full flex-col justify-around rounded-xl bg-quaternary p-6 sm:w-3/5">
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className="flex h-full w-full flex-col justify-around rounded-xl bg-quaternary p-6"
+    >
+      <div className="mb-5 text-center">
+        {notif.message.length > 0 && (
+          <p className={`${errorClass} mb-0 px-1`}>{notif.message}</p>
+        )}
+      </div>
+      <div className="mb-5 flex flex-col">
+        <label htmlFor="name">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Votre nom"
+            autoComplete="off"
+            required
+            className="peer w-full rounded-md border border-gray px-2 py-1 text-black focus:border-terciary focus:outline-none focus:ring-1 focus:ring-terciary "
+          />
+        </label>
+      </div>
       <div className="mb-5 flex flex-col">
         <label htmlFor="email">
           <input
             type="email"
-            id="emailContact"
             name="email"
+            id="email"
             placeholder="Votre email"
             autoComplete="off"
-            onChange={(e) => setEmail(e.target.value)}
-            className="peer w-full rounded-md border border-gray px-2 py-1 text-black invalid:border-red-500 invalid:text-red-500 focus:border-terciary focus:outline-none focus:ring-1 focus:ring-terciary focus:invalid:border-red-500 focus:invalid:ring-red-500"
-            pattern="^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$"
+            required
+            className="w-full rounded-md border border-gray px-2 py-1 text-black focus:border-terciary focus:outline-none focus:ring-1 focus:ring-terciary"
+            // className="peer w-full rounded-md border border-gray px-2 py-1 text-black invalid:border-red-500 invalid:text-red-500 focus:border-terciary focus:outline-none focus:ring-1 focus:ring-terciary focus:invalid:border-red-500 focus:invalid:ring-red-500"
+            // pattern="^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$"
           />
-          <span className="hidden text-sm text-red-500 peer-invalid:block">
-            Vous devez entrer un email valid !
-          </span>
+          {/* <span className="hidden text-sm text-red-500 peer-invalid:block">
+            Vous devez entrer un email valide !
+          </span> */}
         </label>
       </div>
       <div className="mb-5 flex flex-col">
         <label htmlFor="message">
           <textarea
-            id="message"
             name="message"
             placeholder="Votre message"
             autoComplete="off"
-            onChange={(e) => setMessage(e.target.value)}
-            className="peer w-full rounded-md border border-gray px-2 py-1 text-black invalid:border-red-500 invalid:text-red-500 focus:border-terciary focus:outline-none focus:ring-1 focus:ring-terciary focus:invalid:border-red-500 focus:invalid:ring-red-500"
+            rows={5}
+            required
+            className="peer w-full rounded-md border border-gray px-2 py-1 text-black  focus:border-terciary focus:outline-none focus:ring-1 focus:ring-terciary "
           />
         </label>
       </div>
--- client/src/components/sections/Contact.jsx
+++ client/src/components/sections/Contact.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import FormContact from "../FormContact";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
@@ -8,18 +10,46 @@ const Contact = () => {
       className=" mb-8 flex-col items-center justify-center px-2 lg:px-24"
     >
       <h2>Contact</h2>
-      <p>
-        Vous pouvez me contacter par mail à l'adresse suivante :{" "}
-        <a
-          href="mailto:nicomani@hotmail.fr"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-white"
-        >
-          nicomani@hotmail.fr
-        </a>
-      </p>
-      <FormContact />
+      <div className="flex flex-col items-center">
+        <div className="mb-5 flex gap-6">
+          <a
+            href="https://www.linkedin.com/in/nicolas-manigand-a6910b2a6/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              className="hover:animate-rotate-link h-8"
+            />
+          </a>
+          <a
+            href="https://github.com/NicolasM-83200"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              className="hover:animate-rotate-link h-8"
+            />
+          </a>
+        </div>
+        <p className="mb-1">
+          Vous pouvez me contacter par mail à l'adresse suivante :{" "}
+          <a
+            href="mailto:manigand.nicolas@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white"
+          >
+            manigand.nicolas@gmail.com
+          </a>
+        </p>
+        <span>OU</span>
+        <p className="mb-2">
+          Vous pouvez également me contacter via le formulaire ci-dessous :
+        </p>
+        <FormContact />
+      </div>
     </section>
   );
 };
