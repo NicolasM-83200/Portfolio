44e809da2ad9675b0ffc1ff1dfc32a9411144725
Nicolas Manigand <130040163+NicolasM-83200@users.noreply.github.com>
2024-02-10 09:56:37 +0100

    Update GalleryProjects.jsx
    responsive loader

--- client/src/components/GalleryProjects.jsx
+++ client/src/components/GalleryProjects.jsx
@@ -35,10 +35,10 @@ const GalleryProjects = () => {
     <>
       {loading ? (
         <div className="flex min-h-36 flex-col items-center justify-center">
-          <h1 className="text-4xl text-quaternary">
+          <p className="text-2xl md:text-4xl text-quaternary">
             Chargement en cours
             <span className="ml-7 inline-block">{loader}</span>{" "}
-          </h1>
+          </p>
         </div>
       ) : (
         <div className="my-5 grid grid-cols-1 items-center gap-6 md:grid-cols-2 xl:grid-cols-3">
