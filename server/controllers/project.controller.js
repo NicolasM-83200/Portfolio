fb452a4eaa411cbb2495bebb28aa60d2e647b51f
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-28 15:38:54 +0100

    update project.controller.js ajout suppression image de aws s3

--- server/controllers/project.controller.js
+++ server/controllers/project.controller.js
@@ -1,27 +1,35 @@
 const Project = require('../models/project.model');
+const { S3Client, DeleteObjectCommand } = require('@aws-sdk/client-s3');
+
+// on crée un client S3
+const s3 = new S3Client({
+  region: process.env.PORTFOLIO_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.PORTFOLIO_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.PORTFOLIO_AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 exports.createProject = async (req, res) => {
   try {
+    // on parse le body pour récupérer l'objet project
     const projectObject = JSON.parse(req.body.project);
-    // const projectObject = req.body;
+    // on supprime l'id et l'userId de l'objet project
     delete projectObject._id;
     delete projectObject.userId;
+    // on crée un nouvel objet project avec les données de l'objet project et l'userId de la requête
     const project = new Project({
       ...projectObject,
       userId: req.auth.userId,
-      // imageUrl: `${req.protocol}://${req.get('host')}/images/${
-      //   req.file.filename
-      // }`,
+      // on génère l'url de l'image à partir du nom du fichier
       imageUrl: `https://${process.env.PORTFOLIO_AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${req.file.filename}`,
     });
+    // on sauvegarde le projet dans la base de données
     await project.save();
-    console.log('Project :', project);
-    console.log('bucketName: ', process.env.AWS_BUCKET_NAME);
-    console.log('fileName: ', req.file.filename);
+    // on envoie une réponse avec un message de succès et le projet créé
     res.status(201).json({ message: 'Projet créé !', project });
   } catch (error) {
     console.log('Error :', error);
-    console.log(req.body.project);
     res.status(400).json({
       error: 'Une erreur est survenue dans le processus de création...',
     });
@@ -30,7 +38,9 @@ exports.createProject = async (req, res) => {
 
 exports.getAllProjects = async (req, res) => {
   try {
+    // on récupère tous les projets de la base de données
     const projects = await Project.find();
+    // on envoie une réponse avec les projets
     res.status(200).json(projects);
   } catch (error) {
     console.log('Error :', error);
@@ -42,6 +52,7 @@ exports.getAllProjects = async (req, res) => {
 
 exports.getOneProject = async (req, res) => {
   try {
+    // on récupère le projet correspondant à l'id de la requête
     const project = await Project.findById(req.params.id);
     res.status(200).json(project);
   } catch (error) {
@@ -52,19 +63,23 @@ exports.getOneProject = async (req, res) => {
 
 exports.modifyProject = async (req, res) => {
   try {
+    // on crée un objet project avec les données de la requête
     const projectObject = req.file
-      ? {
+      ? // si une image est présente dans la requête, on ajoute l'url de l'image à l'objet project
+        {
           ...req.body,
-          imageUrl: `${req.protocol}://${req.get('host')}/images/${
-            req.file.filename
-          }`,
+          imageUrl: `https://${process.env.PORTFOLIO_AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${req.file.filename}`,
         }
-      : { ...req.body };
+      : // sinon on crée un objet project avec les données de la requête
+        { ...req.body };
     delete projectObject._userId;
+    // on récupère le projet correspondant à l'id de la requête
     const project = await Project.findOne({ _id: req.params.id });
+    // si l'userId du projet est différent de l'userId de la requête, on envoie une erreur 403
     if (project.userId !== req.auth.userId) {
       return res.status(403).json({ message: 'Non autorisé' });
     }
+    // on met à jour le projet correspondant à l'id de la requête avec les données de l'objet project
     await Project.updateOne(
       { _id: req.params.id },
       { ...projectObject, _id: req.params.id }
@@ -80,6 +95,15 @@ exports.modifyProject = async (req, res) => {
 
 exports.deleteProject = async (req, res) => {
   try {
+    // on supprime l'image du projet de S3
+    const data = await s3.send(
+      new DeleteObjectCommand({
+        Bucket: process.env.PORTFOLIO_AWS_BUCKET_NAME,
+        Key: req.body.imageUrl.split('/').pop(),
+      })
+    );
+    console.log('Image supprimée :', data);
+    // on récupère le projet correspondant à l'id de la requête
     await Project.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: 'Projet supprimé !' });
   } catch (error) {
