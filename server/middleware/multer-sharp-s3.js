74092b5ece4598129ce9ace3a1debf12dabff708
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-02-28 14:58:38 +0100

    update multer dotenv

--- server/middleware/multer-sharp-s3.js
+++ server/middleware/multer-sharp-s3.js
@@ -4,10 +4,10 @@ const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const AWS = require('aws-sdk');
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION,
+  region: process.env.PORTFOLIO_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.PORTFOLIO_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.PORTFOLIO_AWS_SECRET_ACCESS_KEY,
   },
 });
 
