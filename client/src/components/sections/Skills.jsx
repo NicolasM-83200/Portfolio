8018d3ccd21ca1c1bbe003faa82cdaa1b5e2dd96
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-03-23 14:31:54 +0100

    update section Skills

--- client/src/components/sections/Skills.jsx
+++ client/src/components/sections/Skills.jsx
@@ -20,12 +20,12 @@ const Skills = () => {
         <img src={js} alt="icon javascript" />
       </div>
       <p>
-        J'ai acquis une solide expertise dans un large éventail de technologies
-        essentielles au développement web. Mon savoir-faire s'étend de la
-        construction de l'ossature avec HTML5, la stylisation avec CSS3, jusqu'à
-        la dynamisation des pages avec JavaScript. J'ai approfondi mes
-        compétences en optimisation SEO pour assurer une visibilité optimale des
-        sites que je développe.
+        J&apos;ai acquis une solide expertise dans un large éventail de
+        technologies essentielles au développement web. Mon savoir-faire
+        s&apos;étend de la construction de l&apos;ossature avec HTML5, la
+        stylisation avec CSS3, jusqu&apos;à la dynamisation des pages avec
+        JavaScript. J&apos;ai approfondi mes compétences en optimisation SEO
+        pour assurer une visibilité optimale des sites que je développe.
       </p>
       <div className="flex w-full justify-around">
         <img src={react} alt="icon react" />
@@ -34,9 +34,9 @@ const Skills = () => {
       <p>
         Je maîtrise également des bibliothèques modernes tels que React, qui me
         permettent de créer des interfaces utilisateur interactives et
-        réactives. Cette compétence m'a permis de concevoir des expériences
+        réactives. Cette compétence m&apos;a permis de concevoir des expériences
         utilisateur fluides et hautement performantes, répondant aux normes les
-        plus élevées de l'industrie.
+        plus élevées de l&apos;industrie.
       </p>
       <div className="flex w-full justify-around">
         <img src={node} alt="icon node" />
@@ -44,18 +44,18 @@ const Skills = () => {
         <img src={mongodb} alt="icon mongodb" />
       </div>
       <p>
-        Du côté du back-end, je me suis plongé dans l'écosystème Node.js et
-        Express.js, ce qui m'a permis de construire des applications web
-        robustes et évolutives. J'ai également acquis une expertise dans la
+        Du côté du back-end, je me suis plongé dans l&apos;écosystème Node.js et
+        Express.js, ce qui m&apos;a permis de construire des applications web
+        robustes et évolutives. J&apos;ai également acquis une expertise dans la
         gestion de bases de données en utilisant MongoDB, garantissant une
         manipulation efficace des données et une intégrité optimale.
       </p>
       <p>
-        Ce qui caractérise ma méthode, c'est ma capacité à travailler de manière
-        complète, du front-end au back-end, afin d'assurer une expérience
-        utilisateur intégrée et fluide. Mon parcours de formation m'a doté des
-        compétences nécessaires pour concevoir, développer et optimiser des
-        projets web de A à Z.
+        Ce qui caractérise ma méthode, c&apos;est ma capacité à travailler de
+        manière complète, du front-end au back-end, afin d&apos;assurer une
+        expérience utilisateur intégrée et fluide. Mon parcours de formation
+        m&apos;a doté des compétences nécessaires pour concevoir, développer et
+        optimiser des projets web de A à Z.
       </p>
     </section>
   );
