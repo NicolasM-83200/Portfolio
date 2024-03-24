9cf80645d25a47b48e6e31fab919fbaa3c8dbbb4
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-03-24 15:34:21 +0100

    update Intro (photo profil width et height)

--- client/src/components/sections/Intro.jsx
+++ client/src/components/sections/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
       id="intro"
       className="mt-[50px] h-[calc(100vh-50px)] items-center justify-center"
     >
-      <span className="animate-scale absolute bottom-0 left-1/2 size-[460px] -translate-x-1/2 translate-y-1/2 rounded-full bg-secondary md:-left-0 md:top-0 md:-translate-x-1/2 md:translate-y-1/2"></span>
+      <span className="absolute bottom-0 left-1/2 size-[460px] -translate-x-1/2 translate-y-1/2 animate-scale rounded-full bg-secondary md:-left-0 md:top-0 md:-translate-x-1/2 md:translate-y-1/2"></span>
       <div className="flex flex-col-reverse items-center gap-5 md:flex-row">
         <div className="basis-1/2 animate-trans-left self-center text-white">
           <img
@@ -24,12 +24,13 @@ const Intro = () => {
           />
           <p className="md:mt-6 md:text-left md:text-xl">
             <strong>
-              Je suis passionné par l'innovation, la résolution de problèmes et
-              la création d'expériences en ligne exceptionnelles.
+              Je suis passionné par l&apos;innovation, la résolution de
+              problèmes et la création d&apos;expériences en ligne
+              exceptionnelles.
             </strong>{" "}
             Mon portfolio reflète non seulement mes compétences techniques, mais
-            aussi mon engagement envers l'excellence. Explorez mon travail pour
-            découvrir comment je peux apporter une valeur ajoutée à votre
+            aussi mon engagement envers l&apos;excellence. Explorez mon travail
+            pour découvrir comment je peux apporter une valeur ajoutée à votre
             projet.
           </p>
         </div>
@@ -38,7 +39,9 @@ const Intro = () => {
             id="profileImage"
             className="w-[70%] md:w-auto"
             src={photoProfil}
-            alt=""
+            width={470}
+            height={654}
+            alt="Photo de profil de Nicolas Manigand"
           />
         </div>
       </div>
