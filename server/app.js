f28a7ea7eabdddd9b6e4b6e005011fbc5850c6b6
NicolasM-83200 <manigand.nicolas@gmail.com>
2024-03-24 14:52:54 +0100

    modifie server.js et app.js pour que mongoDB se connecte avant que le serveur ecoute le port

--- server/app.js
+++ server/app.js
@@ -1,19 +1,25 @@
 // Importation des modules
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require("path");
-const cors = require("cors");
-const helmet = require("helmet");
+require('dotenv').config({ path: './config/.env' });
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+const helmet = require('helmet');
+const startServer = require('./server');
 
 // Importation des routes
-const projectRoute = require("./routes/project.routes");
-const userRoutes = require("./routes/user.routes");
+const projectRoute = require('./routes/project.routes');
+const userRoutes = require('./routes/user.routes');
 
 // Connexion à la base de données MongoDB
 mongoose
   .connect(process.env.URI_MONGODB, { dbName: `${process.env.DB}` })
-  .then(() => console.log("Connexion à MongoDB réussie !"))
-  .catch(() => console.log("Connexion à MongoDB échouée !"));
+  .then(() => {
+    console.log('Connexion à MongoDB réussie !');
+    const port = process.env.PORT || '3000';
+    startServer(port);
+  })
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 // Création de l'application Express
 const app = express();
@@ -28,10 +34,10 @@ app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(cors());
 
 // Middleware qui permet de servir les fichiers statiques du dossier images
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 // Middleware qui permet de servir les routes dédiées aux livres et aux utilisateurs
-app.use("/api/projects", projectRoute);
-app.use("/api/auth", userRoutes);
+app.use('/api/projects', projectRoute);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,51 +1,55 @@
-require("dotenv").config({ path: "./config/.env" });
-const http = require("http");
-const app = require("./app");
+const http = require('http');
+const app = require('./app');
 
-// Normalise le port d'écoute du serveur
-const normalizePort = (val) => {
-  const port = parseInt(val, 10);
+const startServer = async () => {
+  // Normalise le port d'écoute du serveur
+  const normalizePort = (val) => {
+    const port = parseInt(val, 10);
 
-  if (isNaN(port)) return val;
-  if (port >= 0) return port;
+    if (isNaN(port)) return val;
+    if (port >= 0) return port;
 
-  return false;
+    return false;
+  };
+
+  // Définition du port d'écoute du serveur
+  const port = normalizePort(process.env.PORT || '3000');
+  app.set('port', port);
+
+  // Gestion des erreurs
+  const errorHandler = (error) => {
+    if (error.syscall !== 'listen') throw error;
+    const address = server.address();
+    const bind =
+      typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges.');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use.');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+
+  // Création du serveur
+  const server = http.createServer(app);
+
+  // Gestionnaire d'évènements
+  server.on('error', errorHandler);
+  server.on('listening', () => {
+    const address = server.address();
+    const bind =
+      typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+  });
+
+  // Le serveur écoute le port défini
+  server.listen(port);
 };
 
-// Définition du port d'écoute du serveur
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
-
-// Gestion des erreurs
-const errorHandler = (error) => {
-  if (error.syscall !== "listen") throw error;
-  const address = server.address();
-  const bind =
-    typeof address === "string" ? "pipe " + address : "port: " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges.");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use.");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-// Création du serveur
-const server = http.createServer(app);
-
-// Gestionnaire d'évènements
-server.on("error", errorHandler);
-server.on("listening", () => {
-  const address = server.address();
-  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
-  console.log("Listening on " + bind);
-});
-
-// Le serveur écoute le port défini
-server.listen(port);
+module.exports = startServer;
